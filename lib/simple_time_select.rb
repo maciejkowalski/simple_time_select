4f194d08e711382e59cd6236906c474694cc1cba
Maciej Kowalski <mkoval33@gmail.com>
2011-07-29 11:13:11 +0200

    Fixed typo

--- lib/simple_time_select.rb
+++ lib/simple_time_select.rb
@@ -40,7 +40,7 @@ module ActionView::Helpers
               minute_padded = zero_pad_num(minute%60)
               val = "#{hour}:#{minute_padded}:00"
 
-	      if @options[:use_24h]
+	      if @options[:use_24h].eql? true
 		ampm = ""
                 hour_padded = zero_pad_num(hour)
 	      else
